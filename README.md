88d0d4424e1e592197110b27f60d1a5d21990944
Constantin Craciun <constantin@pop-os.localdomain>
2022-08-19 10:53:57 +0300

    ubuntu 22.04 script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # linux_post_install
-Post install scripts for quick deploying our favorite tools
+Post install scripts for quick deploying our favorite tools.
+
+Warning: You should not run sh files without reading and understanding them. 
+
+We use this file or internal quick deploy of workstations and its handy for us to have it here, but do your do dilligence before or fork and adapt to your own needs.
+
+To run on a new machine just copy the command in the termimal:
+
+# Ubuntu 22.04
+```
+wget -O - https://raw.githubusercontent.com/Zalmotek/linux_post_install/main/ubuntu_22.04.sh | sudo bash
+```
---
+++ ubuntu_22.04_dev.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Print out Linux distribution and kernel version
+cat /etc/*-release
+uname -a
+
+# Apps to Install Script
+
+# System Update
+sudo apt update
+sudo apt install -y ubuntu-restricted-extras gnome-tweaks gnome-shell-extension-manager
+
+# Apps
+sudo apt install -y zsh terminator git curl
+sudo apt install -y vlc gimp inkscape ffmpeg
+sudo apt install -y build-essential python3-pip virtualenv npm
+
+##Google Chrome
+cd Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+##Zoom
+
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+
+## Arduino CLI
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+
+# Node 18
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Create basic folders
+mkdir dev
+mkdir tools
+
+# Arduino
+sudo snap install arduino
+sudo usermod -a -G dialout $USER
+
+## Kicad 6.0
+sudo add-apt-repository --yes ppa:kicad/kicad-6.0-releases
+sudo apt update
+sudo apt install --install-recommends -y kicad
+
+# Finishing Things Up
+## System Update and Upgrade
+sudo apt update
+sudo apt upgrade --allow-downgrades -y
+sudo apt full-upgrade --allow-downgrades -y
+## System Clean Up
+sudo apt install -f
+sudo apt autoremove -y
+sudo apt autoclean
+sudo apt clean
+
+# End of Script
+echo "All done :)" 
+
+# sudo reboot
+
